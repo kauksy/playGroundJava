6e43afaf2836a73f4dd46e4db25be0af7371bea2
sy.kim <kauksy9022@gmail.com>
2019-11-13 19:49:16 +0900

    [Revise] add println

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("connect github!");
+		
+		System.out.println("반복학습을 통해 git 사용에 익숙해지자");
+		System.out.println("반복학습을 통해 프로그래밍 숙련도를 높이자");
 	}
 
 }
