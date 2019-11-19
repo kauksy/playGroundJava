fd9a63efef5c9f35950e68b797e974b480d4ba52
sy.kim <kauksy9022@gmail.com>
2019-11-19 15:05:54 +0900

    [Update] printf문으로 입력 받기

--- src/calendar/Sum.java
+++ src/calendar/Sum.java
@@ -10,7 +10,8 @@ public class Sum {
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		System.out.println("1. 두 수의 합은 " + (a + b) + " 입니다.");
-
+		System.out.println();
+		
 		System.out.println("2. 두 수를 입력하세요.");
 		int num1, num2;
 		String str1, str2;
@@ -20,7 +21,10 @@ public class Sum {
 
 		num1 = Integer.parseInt(str1);
 		num2 = Integer.parseInt(str2);
-		System.out.println("2. 두 수의 합은 " + (num1 + num2) + " 입니다.");
+		// %d로 정수로 넣기
+		System.out.printf("2. 두 수의 합은 %d 입니다.", num1 + num2);
+		System.out.println();
+		System.out.printf("두 수 %d, %d의 합은 %d 입니다.", num1, num2, num1 + num2);
 	}
 
 }
