d6167ecc3bfade10c64620e9539e4a894dce74ce
sy.kim <kauksy9022@gmail.com>
2019-11-20 16:10:27 +0900

    [Update]: 프로그램 수행을 위한 main메소드 수정

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,12 +1,40 @@
 package calendar;
 
+import java.util.Scanner;
+
 /**
- * 숫자를 입력 받아 해당하는 달의 최대 일수를 출력하는 프로그램
+ * 가상의 달력찍기
  */
 public class Calendar {
 
+	private static Scanner scan = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		CalendarFactory.exitCondition();
+
+		while (true) {
+			System.out.println("월을 입력하세요.");
+			System.out.print(CalendarFactory.PROMPT);
+			int month = scan.nextInt();
+			if (month == -1) {
+				break;
+			}
+
+			int calendar = CalendarFactory.getMaxDaysOfMonth(month);
+
+			if (calendar == 28) {
+				CalendarFactory.print_28Calendar();
+			}
+
+			if (calendar == 30) {
+				CalendarFactory.print_30Calendar();
+			}
+
+			if (calendar == 31) {
+				CalendarFactory.print_31Calendar();
+			}
+		}
+		
+		System.out.println("Bye~");
 	}
 
 }
