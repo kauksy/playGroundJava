7441a5df44c386018513cfee05055e09956415e4
sy.kim <kauksy9022@gmail.com>
2019-11-21 10:49:28 +0900

    [Update] 동영상 보며 코딩(선생님 코딩)

--- src/calendar/exam/Calendar.java
+++ src/calendar/exam/Calendar.java
@@ -4,32 +4,47 @@ public class Calendar {
 
 	public static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	public static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	
+
 	public static int getMaxDaysOfMonth(int year, int month) {
-		if(isLeapYear(year)){
+		if (isLeapYear(year)) {
 			return LEAP_MAX_DAYS[month - 1];
 		}
 		return MAX_DAYS[month - 1];
 	}
-	
-	public static boolean isLeapYear(int year){
-		if( year % 4 == 0 && ( year % 100 != 0 || year % 400 == 0)){
+
+	public static boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 			return true;
 		}
 		return false;
 	}
 
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 
 		System.out.printf("<< %4d년  %3d월 >>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("----------------------------");
 
-		int maxDay = getMaxDaysOfMonth(year, month);
+		// print blank space
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
 
-		for (int i = 1; i <= maxDay; i++) {
+		int maxDay = getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = count < 7 ? count : 0;
+
+		// print first line
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+		}
+		System.out.println();
+
+		count++;
+		// print second line
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim) {
 				System.out.println();
 			}
 		}
