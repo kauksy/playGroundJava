960f073f83c784e92fe30ad6b2b005b10b3b47fa
sy.kim <kauksy9022@gmail.com>
2019-11-21 10:09:00 +0900

    [Update]: 더 정교한 달력 찍기 - 내 방식대로 구현 - Map을 활용하여 구현

--- src/calendar/CalendarExam.java
+++ src/calendar/CalendarExam.java
@@ -6,19 +6,27 @@ public class CalendarExam {
 
 	private static Scanner scan = new Scanner(System.in);
 	private static MyCalendar myCalendar = new MyCalendar();
+	private static final int WEEK = 7;
+	private static final String SPACE = "   ";
 
 	public static void main(String[] args) {
 
 		int year = -1;
 		int month = -1;
+		String day;
 
 		while (true) {
 			System.out.println("년도를 입력해주세요.");
 			System.out.print("Year> ");
 			year = scan.nextInt();
+
 			System.out.println("월을 입력해주세요.");
 			System.out.print("Month> ");
 			month = scan.nextInt();
+
+			System.out.println("첫번째 요일을 입력하세요. (SU, MO, WE, TH, FR, SA)");
+			System.out.print("WeekDay> ");
+			day = scan.next();
 
 			if (year == -1 || month == -1) {
 				break;
@@ -26,10 +34,18 @@ public class CalendarExam {
 
 			int maxDay = myCalendar.getMonthOfMaxDays(year, month);
 			myCalendar.calendarFrame(year, month);
-			
-			for(int i = 1; i <= maxDay; i++){
+
+			int firstDay = myCalendar.getFirstDay(day);
+
+			for (int i = 0; i < firstDay; i++) {
+				System.out.printf(SPACE);
+			}
+
+			int nextWeek = WEEK - firstDay;
+
+			for (int i = 1; i <= maxDay; i++) {
 				System.out.printf("%3d", i);
-				if(i%7 ==0){
+				if (i % 7 == nextWeek) {
 					System.out.printf("\n");
 				}
 			}
--- src/calendar/MyCalendar.java
+++ src/calendar/MyCalendar.java
@@ -1,9 +1,23 @@
 package calendar;
 
+import java.util.*;
+
 public class MyCalendar {
 
 	public static int[] BASIC_MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	public static int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	private static final Map<String, Integer> firstDay;
+	static {
+		firstDay = new HashMap<String, Integer>();
+		firstDay.put("SU", 1);
+		firstDay.put("MO", 2);
+		firstDay.put("TU", 3);
+		firstDay.put("WE", 4);
+		firstDay.put("TH", 5);
+		firstDay.put("FR", 6);
+		firstDay.put("SA", 7);
+	}
 
 	public void calendarFrame(int year, int month) {
 		System.out.printf(" [ %4d년 %3d월 ]\n", year, month);
@@ -25,7 +39,11 @@ public class MyCalendar {
 		if (checkLeapYear) {
 			return LEAP_MAX_DAYS[month - 1];
 		}
-		
+
 		return BASIC_MAX_DAYS[month - 1];
+	}
+
+	public int getFirstDay(String day) {
+		return (firstDay.get(day) - 1);
 	}
 }
