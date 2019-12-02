7bb83d10af0220497a823b23e5f571e377f31006
sy.kim <kauksy9022@gmail.com>
2019-12-02 16:15:26 +0900

    [Update] make standardCalendar Method
    - 기준일을 설정한다. 1970. 1. 1. THU

--- src/calendar/MyCalendar.java
+++ src/calendar/MyCalendar.java
@@ -4,19 +4,21 @@ import java.util.*;
 
 public class MyCalendar {
 
-	public static int[] BASIC_MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	public static int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	public static int[] BASIC_MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	public static int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	private static final Map<String, Integer> firstDay;
+	private static final Map<Integer, String> firstDay;
+
 	static {
-		firstDay = new HashMap<String, Integer>();
-		firstDay.put("SU", 1);
-		firstDay.put("MO", 2);
-		firstDay.put("TU", 3);
-		firstDay.put("WE", 4);
-		firstDay.put("TH", 5);
-		firstDay.put("FR", 6);
-		firstDay.put("SA", 7);
+		firstDay = new HashMap<Integer, String>();
+		firstDay.put(1, "SU");
+		firstDay.put(2, "MO");
+		firstDay.put(3, "TU");
+		firstDay.put(4, "WE");
+		firstDay.put(5, "TH");
+		firstDay.put(6, "FR");
+		firstDay.put(7, "SA");
+
 	}
 
 	public void calendarFrame(int year, int month) {
@@ -37,13 +39,36 @@ public class MyCalendar {
 		boolean checkLeapYear = checkLeapYear(year);
 
 		if (checkLeapYear) {
-			return LEAP_MAX_DAYS[month - 1];
+			return LEAP_MAX_DAYS[month];
 		}
 
-		return BASIC_MAX_DAYS[month - 1];
+		return BASIC_MAX_DAYS[month];
 	}
 
-	public int getFirstDay(String day) {
-		return (firstDay.get(day) - 1);
+	public String getFirstDay(int day) {
+		return (firstDay.get(day));
+	}
+
+	public int standardCalendar(int year, int month) {
+		// 1970. 01. 01 THUR 를 기준으로 한다.
+		int standardYear = 1970;
+		int standardMonth = 1;
+		int standardWeekday = 5;
+
+		int count = 0;
+		for (int i = standardYear; i < year; i++) {
+			int distance = checkLeapYear(i) ? 366 : 365;
+			count += distance;
+		}
+
+		for (int i = standardMonth; i < month; i++) {
+			int distance = getMonthOfMaxDays(year, i);
+			count += distance;
+		}
+
+		count = count - 1;
+
+		int resultWeekday = (count + standardWeekday) % 7;
+		return resultWeekday;
 	}
 }
